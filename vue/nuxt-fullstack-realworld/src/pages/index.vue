22082a365b9b6f80178301b135100abc224a8878
mikinovation <maneuver2472@gmail.com>
2024-04-10 23:41:50 +0900

    feat: add tags menu

--- vue/nuxt-fullstack-realworld/src/pages/index.vue
+++ vue/nuxt-fullstack-realworld/src/pages/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 // TODO: Add constant title
+
+const articles = ref([]);
+const tags = ref([]);
+const tagLoading = ref(false);
 </script>
 
 <template>
@@ -14,6 +18,41 @@
           conduit
         </h1>
         <p class="font-thin text-xl pb-4">A place to share your knowledge.</p>
+      </TheContainer>
+      <TheContainer>
+        <div class="flex md:space-x-4">
+          <div class="md:w-3/4 mr-4">
+            <!-- TabbedMenu -->
+            <p v-if="articles.length === 0">Loading articles...</p>
+            <div v-else>
+              <p v-if="articles.length === 0">No articles are here... yet.</p>
+              <!-- TODO: ArticleList -->
+              <!-- TODO: Pagination -->
+            </div>
+          </div>
+        </div>
+
+        <div class="md:w-1/4 mt-2">
+          <div class="sidebar p-3 rounded bg-gray-100">
+            <p class="mb-2 text-sm font-medium">Popular Tags</p>
+            <p v-if="tagLoading">Loading tags...</p>
+            <ul v-else-if="tags.length > 0" class="tag-list list-none">
+              <li
+                v-for="tag in tags"
+                :key="tag"
+                class="tag-default tag-pill tag-outline"
+              >
+                <NuxtLink to="/">
+                  <span
+                    class="border border-gray-300 text-xs py-0.5 whitespace-nowrap inline-block px-2.5 rounded-full text-white bg-gray-400"
+                  >
+                    {{ tag }}
+                  </span>
+                </NuxtLink>
+              </li>
+            </ul>
+          </div>
+        </div>
       </TheContainer>
     </div>
   </PageContainer>
