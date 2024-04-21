889a45689c133a118ada46e7318c2c0494491c0d
mikinovation <maneuver2472@gmail.com>
2024-04-21 21:02:11 +0900

    feat: add schema

--- vue/nuxt-fullstack-realworld/packages/api/db/schema.ts
+++ vue/nuxt-fullstack-realworld/packages/api/db/schema.ts
@@ -1,11 +1,42 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+
+const idSchema = uuid("id").defaultRandom().notNull().primaryKey();
+
+const timestamptz = (columnName: string) =>
+  timestamp(columnName, { withTimezone: true });
+
+const createdAtSchema = timestamptz("created_at").defaultNow().notNull();
+
+const updatedAtSchema = timestamptz("updated_at").defaultNow().notNull();
 
 export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
+  id: idSchema,
   email: text("email").unique().notNull(),
   password: text("password").notNull(),
   image: text("image"),
   bio: text("bio"),
-  createdAt: text("created_at").notNull(),
-  updatedAt: text("updated_at").notNull(),
+  createdAt: createdAtSchema,
+  updatedAt: updatedAtSchema,
+});
+
+export const follows = pgTable("follows", {
+  id: idSchema,
+  followerId: uuid("follower_id")
+    .notNull()
+    .references(() => users.id),
+  followingId: uuid("following_id")
+    .notNull()
+    .references(() => users.id),
+  createdAt: createdAtSchema,
+  updatedAt: updatedAtSchema,
+});
+
+export const articles = pgTable("articles", {
+  id: idSchema,
+  slug: text("slug").unique().notNull(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  body: text("body").notNull(),
+  createdAt: createdAtSchema,
+  updatedAt: updatedAtSchema,
 });
