6eb41c0b72820f28b496605a98eefe803104d0b1
mikinovation <maneuver2472@gmail.com>
2024-04-22 16:19:15 +0900

    feat: add datepicker test

---
+++ vue/nuxt-vuedatepicker/app.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+const date = ref(new Date());
+</script>
+
+<template>
+  <VueDatePicker v-model="date">
+    <template #clear-icon="{ clear }">
+      <button @click="clear">Clear</button>
+    </template>
+  </VueDatePicker>
+</template>
